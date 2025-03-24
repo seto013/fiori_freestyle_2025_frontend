f23c4c75910b87a343aec221277dc37ecefcae47
seto013 <marcos.seto@gmail.com>
2025-03-24 12:15:46 -0300

    first commit 202503241215

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("zov.controller.View1", {
+        onInit: function () {
+
+        },
+
+        onPress: function() {
+            alert("Hello World");
+        }
+    });
+});
