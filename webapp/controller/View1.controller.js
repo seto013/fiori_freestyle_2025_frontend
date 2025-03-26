5b59b5e085961b1cac58eb07e43a7e6f6e188276
seto013 <marcos.seto@gmail.com>
2025-03-26 12:17:15 -0300

    Commit 202503261217 026 i18n

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -11,37 +11,20 @@ function (Controller,MessageToast) {
     "use strict";
 
     return Controller.extend("zov.controller.View1", {
-        onInit: function () {
-            // alert("onInit");
+        onInit: function () {     
+            var oView  = this.getView();
+            var oModel = new sap.ui.model.json.JSONModel();
+            oModel.setData({"usuario": {"nome": "Marcos"}});
+            oView.setModel(oModel);       
         },
 
-        onBeforeRendering: function() {
-            // alert("onBeforeRendering");
-        },
+        onExibirMensagem: function() {
+            var oI18n  = this.getView().getModel("i18n").getResourceBundle();
+            var oModel = this.getView().getModel();
+            var oData  = oModel.getData();
 
-        onAfterRendering: function() {
-            // alert("onAfterRendering");
-        },
-
-        onExit: function() {
-            alert("onExit");
-        },
-
-        onCalcular: function() {
-            var oView = this.getView();
-            var iB1 = parseInt(oView.byId("b1").getValue());
-            var iB2 = parseInt(oView.byId("b2").getValue());
-            var iB3 = parseInt(oView.byId("b3").getValue());
-            var iB4 = parseInt(oView.byId("b4").getValue());
-            var fResultado = 0;
-
-            fResultado = ( iB1 + iB2 + iB3 + iB4 ) / 4;
-
-            oView.byId("resultado").setValue(fResultado);
-
-            MessageToast.show("Resultado = "+fResultado);
-
-            oView.destroy();
+            var sText  = oI18n.getText("welcomeMsg",[oData.usuario.nome]);
+            alert(sText);
         }
     });
 });
