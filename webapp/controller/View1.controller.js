233efb43263018f73b786532475af011bca46787
seto013 <marcos.seto@gmail.com>
2025-03-26 15:05:00 -0300

    Commit 202503261504 028 Model

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -11,20 +11,50 @@ function (Controller,MessageToast) {
     "use strict";
 
     return Controller.extend("zov.controller.View1", {
-        onInit: function () {     
+        onInit: function () {
+            // // model padrão da view
+            // var oView  = this.getView();
+            // var oModel = new sap.ui.model.json.JSONModel();
+            // oModel.setData({"usuario": {"nome": "Marcos"}});
+            // oView.setModel(oModel);
+
+            // model com o nome "dados"
             var oView  = this.getView();
             var oModel = new sap.ui.model.json.JSONModel();
-            oModel.setData({"usuario": {"nome": "Marcos"}});
-            oView.setModel(oModel);       
+            oModel.setData({"usuario": {"nome": "Marisa"}});
+            oView.setModel(oModel,"dados");
         },
 
-        onExibirMensagem: function() {
-            var oI18n  = this.getView().getModel("i18n").getResourceBundle();
-            var oModel = this.getView().getModel();
-            var oData  = oModel.getData();
+        onTestModels: function() {
+            // model i18n
+            var oI18n  = this.getView().getModel("i18n").getResourceBundle();            
+            var sText  = oI18n.getText("title");
 
-            var sText  = oI18n.getText("welcomeMsg",[oData.usuario.nome]);
-            alert(sText);
+            console.log("Texto com a chave 'title'");
+            console.log(sText);
+
+            console.log("-------------------------------------------------");
+
+            // model de usuarios
+            var oModel = this.getOwnerComponent().getModel("usuarios");
+            var oData  = oModel.getData();
+            console.log("Model dos usuários");
+            console.log(oData);
+
+            console.log("-------------------------------------------------");
+
+            // model do serviço
+            var oModel = this.getOwnerComponent().getModel();
+            oModel.read("/OVCabSet",{
+                success: function(oData, oResponse){
+                    console.log("Dados retornados do serviço");
+                    console.log(oData);
+                    console.log(oResponse);
+                },
+                error: function(oError){
+                    console.log(oError);
+                }
+            });            
         }
     });
 });
